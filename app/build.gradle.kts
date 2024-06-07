ab45aefc8bf46f3e4179d5695325c6bff1b3aeae
h4de5ing <lex109@qq.com>
2024-06-07 12:42:34 +0800

    打包脚本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,22 @@
+import java.io.ByteArrayOutputStream
+import java.text.SimpleDateFormat
+import java.util.Date
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
 }
+fun runCommand(command: String): String {
+    val byteOut = ByteArrayOutputStream()
+    project.exec {
+        commandLine = command.split(" ")
+        standardOutput = byteOut
+    }
+    return String(byteOut.toByteArray()).trim()
+}
+
+fun getGitSha(): String = runCommand("git rev-parse --short HEAD")
+
 
 android {
     namespace = "com.h4de5ing.autopickupcall"
@@ -42,6 +57,28 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    applicationVariants.configureEach {
+        val buildType = buildType.name
+        val id = defaultConfig.applicationId
+        val prefix = id?.substring(id.lastIndexOf(".") + 1) ?: ""
+        val createTime = SimpleDateFormat("yyyyMMddHHmmss").format(Date())
+        if (buildType == "release") {
+            outputs.all {
+                val fromFile = outputFile
+                val intoFile = "D:/test/$prefix/v${defaultConfig.versionName}"
+//                val intoFile = "\\\\192.168.0.74\\亿道信息\\RD\\SW_A\\71备份过来的\\APP软件版本\\${prefix}\\v${defaultConfig.versionName}"
+                copy {
+                    from(fromFile)
+                    into(intoFile)
+                    include("*release*.apk")
+                    rename(
+                        "app-",
+                        "${prefix}_v${defaultConfig.versionName}_${getGitSha()}_${createTime}_"
+                    )
+                }
+            }
+        }
+    }
 }
 
 dependencies {
@@ -50,9 +87,9 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation("androidx.lifecycle:lifecycle-service:2.8.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:2.8.1")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.0")
+    implementation(libs.lifecycle.service)
+    implementation(libs.lifecycle.viewmodel.ktx)
+    implementation(libs.lifecycle.viewmodel.savedstate)
+    implementation(libs.androidx.lifecycle.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.runtime.compose)
 }
